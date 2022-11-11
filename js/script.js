73e011e8ca7bc2ce0016c53ae1ba3f83bf9fe984
jesusbaby1233 <oluwakemiolanipekun10@gmail.com>
2022-11-12 00:14:51 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// this is a single line javascript comment 
+
+/*
+this is a multiline javascript comment 
+*/
+/*
+javascript is an interpreted scripting or programming language : this means that an interpreter converts it directly into machine readable code and executes it 
+
+javascript is used to make webpaes dynamic; it can be used to manipulate the html document and css styles 
+*/
